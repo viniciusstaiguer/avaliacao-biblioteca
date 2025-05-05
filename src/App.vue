ce24f71402684e033b8c653150946695db2a56b5
viniciusstaiguer <vinistaiguer@gmail.com>
2025-05-05 15:36:26 -0300

    ADD: Importado provider e useStore

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { provide } from 'vue'
+import { useStore } from '@/stores/store.js'
+
+const store = useStore()
+provide('store', store)
 </script>
 <template>
   <RouterView />
